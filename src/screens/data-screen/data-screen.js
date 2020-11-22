1fa98a3a45c6925f7b5d1e789abebcdb0df83ddd
Nicholas Giusti <nicholas.giusti89@gmail.com>
2020-11-21 16:47:57 -0800

    updated data screen to take data

--- src/screens/data-screen/data-screen.js
+++ src/screens/data-screen/data-screen.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import LinkButton from '../../components/UI/link-button/link-button'
 
-const DataScreen = () => (
-    <div className="center-content">
-        <h1>Data</h1>
-        <LinkButton link="/">Home</LinkButton >
-    </div>
-)
+class DataScreen extends Component {
+    render() {
+        return (
+            <div className="center-content">
+                <h1>Data</h1>
+                <h2>Session Type: {this.props.gameType}</h2>
+                <h2>Name: {this.props.name}</h2>
+                <h2>Score: {this.props.score}</h2>
+                <h2>Misses: {this.props.miss}</h2>
+                <LinkButton link="/">Home</LinkButton >
+            </div>
+        )
+    }
 
-export default DataScreen
+}
+
+
+const mapStateToProps = state => {
+    return {
+        name: state.name,
+        shots: state.shots,
+        shotsRemaining: state.shotsRemaining,
+        score: state.score,
+        miss: state.miss,
+        gameType: state.gameType,
+    };
+}
+
+export default connect(mapStateToProps)(DataScreen)
